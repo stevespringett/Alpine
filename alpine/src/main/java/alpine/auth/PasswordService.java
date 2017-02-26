fbc414c9fd5e361bb3e32321def47e80adee1c53
stevespringett <steve@springett.us>
2017-02-26 14:54:37 -0600

    Added method to evaluate need to rehash BCrypt passwords based on an increase in work factor or an invalid hash

--- alpine/src/main/java/alpine/auth/PasswordService.java
+++ alpine/src/main/java/alpine/auth/PasswordService.java
@@ -63,4 +63,31 @@ public class PasswordService {
         return BCrypt.checkpw(new String(assertedPassword), new String(user.getPassword()));
     }
 
+    /**
+     * Checks the asserted BCrypt formatted hashed password and determines if the password should
+     * be rehashed or not. If the BCrypt work factor is increased (from 12 to 14 for example),
+     * passwords should be evaluated and if the existing stored hash uses a work factor less than
+     * what is configured, then the assertedPassword should be rehashed. The same does not apply
+     * in reverse. Stored hashed passwords with a work factor greater than the configured work factor
+     * will return false, meaning they should not be rehashed.
+     *
+     * If the assertedPasswords length is less than the minimum length of a BCrypt hash, this method
+     * will return true.
+     *
+     * @since 1.0.0
+     */
+    public static boolean shouldRehash(char[] assertedPassword) {
+        int rounds;
+        if (assertedPassword.length < 59) {
+            return true;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(assertedPassword[4]);
+        if (assertedPassword[5] != '$') {
+            sb.append(assertedPassword[5]);
+        }
+        rounds = Integer.valueOf(sb.toString());
+        return rounds < ROUNDS;
+    }
+
 }
