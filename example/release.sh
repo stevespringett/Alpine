923ada0ace82b5df0830dfc19e047c027c1ecf0d
Steve Springett <steve@springett.us>
2018-02-24 15:48:20 -0600

    Updated version logic

--- example/release.sh
+++ example/release.sh
@@ -2,6 +2,11 @@
 read -p "Really deploy to maven central repository  (yes/no)? "
 if ( [ "$REPLY" == "yes" ] ) then
 
+read -p "Specify release version number in Maven Central (i.e. 1.0.0): "
+
+mvn versions:set -DnewVersion=$REPLY
+mvn clean deploy -Prelease
+mvn versions:revert
 mvn release:clean release:prepare release:perform -Prelease -X -e | tee maven-central-deploy.log
 
 else
