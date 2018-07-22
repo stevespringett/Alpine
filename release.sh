6d0af2cbaeca57e5a8bef02bd8393b6cdd5529cb
Steve Springett <steve@springett.us>
2018-07-21 23:26:48 -0500

    Pinned to java 8

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export PATH=JAVA_HOME/bin:$PATH
+
 read -p "Really deploy to Maven Central repository (Y/N)? "
 if ( [ "$REPLY" == "Y" ] ) then
 
