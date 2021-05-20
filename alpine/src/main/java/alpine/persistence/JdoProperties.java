4b72d8fa9a4f35314ae66db972d29c860cd39ee7
Steve Springett <steve@springett.us>
2021-05-20 00:05:20 -0500

    Added support for minimum number of connections to keep idle in the connection pool. https://github.com/DependencyTrack/dependency-track/issues/941

--- alpine/src/main/java/alpine/persistence/JdoProperties.java
+++ alpine/src/main/java/alpine/persistence/JdoProperties.java
@@ -21,6 +21,18 @@ package alpine.persistence;
 import alpine.Config;
 import java.util.Properties;
 
+import static org.datanucleus.PropertyNames.PROPERTY_CONNECTION_POOLINGTYPE;
+import static org.datanucleus.PropertyNames.PROPERTY_QUERY_JDOQL_ALLOWALL;
+import static org.datanucleus.PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_COLUMNS;
+import static org.datanucleus.PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS;
+import static org.datanucleus.PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_DATABASE;
+import static org.datanucleus.PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_TABLES;
+import static org.datanucleus.PropertyNames.PROPERTY_SCHEMA_GENERATE_DATABASE_MODE;
+import static org.datanucleus.store.rdbms.RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_IDLE_TIMEOUT;
+import static org.datanucleus.store.rdbms.RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MAX_LIFETIME;
+import static org.datanucleus.store.rdbms.RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MAX_POOL_SIZE;
+import static org.datanucleus.store.rdbms.RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MIN_IDLE;
+
 /**
  * This class provides runtime constants for JDO properties.
  *
@@ -37,17 +49,18 @@ public final class JdoProperties {
         properties.put("javax.jdo.option.ConnectionUserName", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_USERNAME));
         properties.put("javax.jdo.option.ConnectionPassword", Config.getInstance().getPropertyOrFile(Config.AlpineKey.DATABASE_PASSWORD));
         if (Config.getInstance().getPropertyAsBoolean(Config.AlpineKey.DATABASE_POOL_ENABLED)) {
-            properties.put("datanucleus.connectionPoolingType", "HikariCP");
-            properties.put("datanucleus.connectionPool.maxPoolSize", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_SIZE));
-            properties.put("datanucleus.connectionPool.idleTimeout", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_IDLE_TIMEOUT));
-            properties.put("datanucleus.connectionPool.maxLifetime", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_LIFETIME));
+            properties.put(PROPERTY_CONNECTION_POOLINGTYPE, "HikariCP");
+            properties.put(PROPERTY_CONNECTION_POOL_MAX_POOL_SIZE, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_SIZE));
+            properties.put(PROPERTY_CONNECTION_POOL_IDLE_TIMEOUT, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_IDLE_TIMEOUT));
+            properties.put(PROPERTY_CONNECTION_POOL_MAX_LIFETIME, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_LIFETIME));
+            properties.put(PROPERTY_CONNECTION_POOL_MIN_IDLE, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MIN_IDLE));
         }
-        properties.put("datanucleus.schema.autoCreateDatabase", "true");
-        properties.put("datanucleus.schema.autoCreateTables", "true");
-        properties.put("datanucleus.schema.autoCreateColumns", "true");
-        properties.put("datanucleus.schema.autoCreateConstraints", "true");
-        properties.put("datanucleus.generateSchema.database.mode", "create");
-        properties.put("datanucleus.query.jdoql.allowAll", "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_DATABASE, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_TABLES, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_COLUMNS, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS, "true");
+        properties.put(PROPERTY_SCHEMA_GENERATE_DATABASE_MODE, "create");
+        properties.put(PROPERTY_QUERY_JDOQL_ALLOWALL, "true");
         return properties;
     }
 
@@ -59,12 +72,12 @@ public final class JdoProperties {
         properties.put("javax.jdo.option.ConnectionUserName", "sa");
         properties.put("javax.jdo.option.ConnectionPassword", "");
         properties.put("javax.jdo.option.Mapping", "h2");
-        properties.put("datanucleus.schema.autoCreateDatabase", "true");
-        properties.put("datanucleus.schema.autoCreateTables", "true");
-        properties.put("datanucleus.schema.autoCreateColumns", "true");
-        properties.put("datanucleus.schema.autoCreateConstraints", "true");
-        properties.put("datanucleus.generateSchema.database.mode", "create");
-        properties.put("datanucleus.query.jdoql.allowAll", "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_DATABASE, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_TABLES, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_COLUMNS, "true");
+        properties.put(PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS, "true");
+        properties.put(PROPERTY_SCHEMA_GENERATE_DATABASE_MODE, "create");
+        properties.put(PROPERTY_QUERY_JDOQL_ALLOWALL, "true");
         return properties;
     }
 }
