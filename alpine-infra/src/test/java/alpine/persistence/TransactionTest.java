c4f97b4c22b2f11180afa9d717f3c0bc961c2264
nscuro <nscuro@protonmail.com>
2024-04-20 16:47:25 +0200

    Use lambdas for transaction scoping; Don't reload objects after commit
    The way transactions were previously handled made it impossible to compose multiple transactional operations into larger tasks. `PersistenceManager#currentTransaction()#begin()` fails when the transaction is already active. This in turn opens the door to data inconsistencies, when parts of a task are committed but later operations fail, with no way to roll back to the original state before the task was started.
    
    It also encouraged commits in high frequencies, which can take a toll on the database's write-ahead-log for high volume processing use-cases.
    
    There was no handling of rollbacks, such that a failed commit could have "poisoned" an entire `PersistenceManager`, as no further persistence operations can be performed until after a rollback was executed.
    
    This change introduces the `Transaction` class, which coordinates transaction begin, commit, and rollback. It is also capable of handling nested transactions, by joining an existing active one if required. This aspect can be controlled via propagation settings.
    
    It also removes the need to reload objects from the database after each commit. This behavior has historically added quite a big overhead, and apparently was needed because DataNucleus transitions objects into "hollow" state (all fields except the primary key are unloaded). The unloading of fields is now disabled globally by setting the `DataNucleus.RetainValues` property to `true`.
    
    The `ScopedCustomization` class was added to allow for scoped changes to a `PersistenceManager`, without impacting later operations on it. Usually things like adding fetch groups via `pm.getFetchPlan().addGroup("foo")` are intended to only affect one or a handful of queries, but in reality it affects all queries for the lifetime of the `PersistenceManager`. This has historically led to over-fetching. Now, it's easier to scope such things more tightly.
    
    Further, this PR makes an effort to ensure that all `Query` objects are closed once consumed. Not closing query objects unnecessary leaks resources in cases where the owning `PersistenceManager` is **not** short-lived.
    
    References:
    
    * https://www.datanucleus.org/products/accessplatform_6_0/jdo/persistence.html#lifecycle
    * https://www.datanucleus.org/products/accessplatform_6_0/jdo/query.html#_closing_a_query
    * https://www.datanucleus.org/products/accessplatform_6_0/jdo/persistence.html#transaction_local
    * https://github.com/search?q=repo%3AGoogleCloudPlatform%2Fdatanucleus-appengine%20retainvalues&type=code
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

---
+++ alpine-infra/src/test/java/alpine/persistence/TransactionTest.java
@@ -0,0 +1,180 @@
+/*
+ * This file is part of Alpine.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package alpine.persistence;
+
+import alpine.model.ManagedUser;
+import alpine.model.Team;
+import alpine.persistence.Transaction.Isolation;
+import alpine.persistence.Transaction.Options;
+import alpine.persistence.Transaction.Propagation;
+import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.jdo.JDOHelper;
+
+import static alpine.persistence.Transaction.defaultOptions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class TransactionTest {
+
+    private JDOPersistenceManagerFactory pmf;
+    private AlpineQueryManager qm;
+
+    @Before
+    public void setUp() {
+        pmf = (JDOPersistenceManagerFactory) JDOHelper.getPersistenceManagerFactory(JdoProperties.unit(), "Alpine");
+        qm = new AlpineQueryManager(pmf.getPersistenceManager());
+    }
+
+    @After
+    public void tearDown() {
+        if (qm != null) {
+            qm.close();
+        }
+
+        if (pmf != null) {
+            pmf.close();
+        }
+    }
+
+    @Test
+    public void testRetainValues() {
+        final Team team = qm.callInTransaction(() -> qm.createTeam("foo", true));
+        qm.close(); // Close PM to prevent lazy loading of values when getters are called.
+
+        // Ensure the values assigned during the transaction are present.
+        assertThat(team.getName()).isEqualTo("foo");
+        assertThat(team.getApiKeys()).satisfiesExactly(apiKey -> assertThat(apiKey.getKey()).isNotNull());
+    }
+
+    @Test
+    public void testTransactionRollback() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> qm.runInTransaction(() -> {
+                    final ManagedUser user = qm.createManagedUser("username", "passwordHash");
+                    final Team team = qm.createTeam("foo", true);
+                    final boolean added = qm.addUserToTeam(user, team);
+                    assertThat(added).isTrue();
+
+                    throw new IllegalStateException();
+                }))
+                .withCauseInstanceOf(IllegalStateException.class);
+
+        // Changes made in the transaction must have been rolled back.
+        assertThat(qm.getManagedUser("username")).isNull();
+        assertThat(qm.getTeam("foo")).isNull();
+    }
+
+    @Test
+    public void testNestedTransactionRollback() {
+        qm.runInTransaction(() -> {
+            final ManagedUser userA = qm.createManagedUser("usernameA", "passwordHash");
+            final ManagedUser userB = qm.createManagedUser("usernameB", "passwordHash");
+            final Team team = qm.createTeam("foo", true);
+
+            final boolean addedUserA = qm.addUserToTeam(userA, team);
+            assertThat(addedUserA).isTrue();
+
+            // Run the addition of userB to the team in a nested transaction.
+            // The transaction should join the currently active one.
+            // Throw an exception at the end. The exception must not cause
+            // the transaction to be rolled back.
+            assertThatExceptionOfType(RuntimeException.class)
+                    .isThrownBy(() -> qm.runInTransaction(() -> {
+                        final boolean addedUserB = qm.addUserToTeam(userB, team);
+                        assertThat(addedUserB).isTrue();
+
+                        throw new IllegalStateException();
+                    }));
+        });
+
+        final ManagedUser userA = qm.getManagedUser("usernameA");
+        assertThat(userA).isNotNull();
+        assertThat(userA.getTeams()).hasSize(1);
+
+        final ManagedUser userB = qm.getManagedUser("usernameB");
+        assertThat(userB).isNotNull();
+        assertThat(userB.getTeams()).hasSize(1);
+    }
+
+    @Test
+    public void testNestedTransactionWithRequiresNewPropagation() {
+        qm.runInTransaction(() ->
+                assertThatExceptionOfType(IllegalStateException.class)
+                        .isThrownBy(() -> {
+                            final Options trxOptions = defaultOptions().withPropagation(Propagation.REQUIRES_NEW);
+                            qm.runInTransaction(trxOptions, () -> {
+                            });
+                        })
+                        .withMessage("Propagation is set to REQUIRES_NEW, but a transaction is already active")
+        );
+    }
+
+    @Test
+    public void testNestedTransactionWithIsolationMismatch() {
+        qm.runInTransaction(() ->
+                assertThatExceptionOfType(IllegalStateException.class)
+                        .isThrownBy(() -> {
+                            final Options trxOptions = defaultOptions().withIsolation(Isolation.SERIALIZABLE);
+                            qm.runInTransaction(trxOptions, () -> {
+                            });
+                        })
+                        .withMessage("""
+                                Requested isolation is SERIALIZABLE, but transaction is already \
+                                active with isolation READ_COMMITTED""")
+        );
+    }
+
+    @Test
+    public void testNestedTransactionWithSerializeReadMismatch() {
+        qm.runInTransaction(() ->
+                assertThatExceptionOfType(IllegalStateException.class)
+                        .isThrownBy(() -> {
+                            final Options trxOptions = defaultOptions().withSerializeRead(true);
+                            qm.runInTransaction(trxOptions, () -> {
+                            });
+                        })
+                        .withMessage("""
+                                Requested serializeRead=true, but transaction is already \
+                                active with serializeRead=false""")
+        );
+    }
+
+    @Test
+    public void testIsolationRestore() {
+        final Options trxOptions = defaultOptions().withIsolation(Isolation.SERIALIZABLE);
+        qm.runInTransaction(trxOptions, () -> {
+        });
+
+        assertThat(qm.getPersistenceManager().currentTransaction().getIsolationLevel()).isEqualTo("read-committed");
+    }
+
+    @Test
+    public void testSerializableReadRestore() {
+        final Options trxOptions = defaultOptions().withSerializeRead(true);
+        qm.runInTransaction(trxOptions, () -> {
+        });
+
+        assertThat(qm.getPersistenceManager().currentTransaction().getSerializeRead()).isFalse();
+    }
+
+}
