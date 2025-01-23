d5a4e8d045c9052d8ca34e0addec84c9418e6b81
Thomas Schauer-Koeckeis <thomas.schauer-koeckeis@rohde-schwarz.com>
2025-01-23 14:00:34 +0100

    Added things from review
    Signed-off-by: Thomas Schauer-Koeckeis <thomas.schauer-koeckeis@rohde-schwarz.com>

--- alpine-server/src/test/java/alpine/server/auth/ApiKeyAuthenticationServiceTest.java
+++ alpine-server/src/test/java/alpine/server/auth/ApiKeyAuthenticationServiceTest.java
@@ -62,7 +62,7 @@ public class ApiKeyAuthenticationServiceTest {
         }
         final ContainerRequest containerRequestMock = Mockito.mock(ContainerRequest.class);
         Mockito.when(containerRequestMock.getHeaderString("X-Api-Key"))
-                .thenReturn(apiKey.getKey());
+                .thenReturn(apiKey.getClearTextKey());
         final ApiKeyAuthenticationService authService = new ApiKeyAuthenticationService(containerRequestMock, false);
 
         final ApiKey authenticatedUser = (ApiKey) authService.authenticate();
@@ -80,7 +80,7 @@ public class ApiKeyAuthenticationServiceTest {
         }
         final ContainerRequest containerRequestMock = Mockito.mock(ContainerRequest.class);
         Mockito.when(containerRequestMock.getHeaderString("X-Api-Key"))
-                .thenReturn(apiKey.getKey());
+                .thenReturn(apiKey.getClearTextKey());
         final ApiKeyAuthenticationService authService = new ApiKeyAuthenticationService(containerRequestMock, false);
 
         final ApiKey authenticatedUser = (ApiKey) authService.authenticate();
@@ -91,14 +91,16 @@ public class ApiKeyAuthenticationServiceTest {
     @Test
     public void authenticationShouldThrowAuthenticationExceptionForOldKeyAfterRegeneration() {
         ApiKey apiKey;
+        String oldKey;
         try (final AlpineQueryManager qm = new AlpineQueryManager()) {
             final var team = qm.createTeam("Test");
             apiKey = qm.createApiKey(team);
-            qm.regenerateApiKey(apiKey);
+            oldKey = apiKey.getClearTextKey();
+            var a = qm.regenerateApiKey(apiKey);
         }
         final ContainerRequest containerRequestMock = Mockito.mock(ContainerRequest.class);
         Mockito.when(containerRequestMock.getHeaderString("X-Api-Key"))
-                .thenReturn(apiKey.getKey());
+                .thenReturn(oldKey);
         final ApiKeyAuthenticationService authService = new ApiKeyAuthenticationService(containerRequestMock, false);
 
         Assertions.assertThatExceptionOfType(AuthenticationException.class)
@@ -161,7 +163,7 @@ public class ApiKeyAuthenticationServiceTest {
         }
         final ContainerRequest containerRequestMock = Mockito.mock(ContainerRequest.class);
         Mockito.when(containerRequestMock.getHeaderString("X-Api-Key"))
-                .thenReturn(apiKey.getKey() + "1");
+                .thenReturn(apiKey.getClearTextKey() + "1");
         final ApiKeyAuthenticationService authService = new ApiKeyAuthenticationService(containerRequestMock, false);
 
         Assertions.assertThatExceptionOfType(AuthenticationException.class)
@@ -173,7 +175,7 @@ public class ApiKeyAuthenticationServiceTest {
         final var apiKey = genLegacyKey();
         final ContainerRequest containerRequestMock = Mockito.mock(ContainerRequest.class);
         Mockito.when(containerRequestMock.getHeaderString("X-Api-Key"))
-                .thenReturn(apiKey.getKey());
+                .thenReturn(apiKey.getClearTextKey());
         final ApiKeyAuthenticationService authService = new ApiKeyAuthenticationService(containerRequestMock, false);
 
         final ApiKey authenticatedUser = (ApiKey) authService.authenticate();
@@ -207,8 +209,8 @@ public class ApiKeyAuthenticationServiceTest {
 
     private ApiKey genLegacyKey() throws NoSuchAlgorithmException {
         final var apiKey = new ApiKey();
-        final String clearKey = ApiKeyGenerator.generate(32);
         try (final AlpineQueryManager qm = new AlpineQueryManager()) {
+            final String clearKey = ApiKeyGenerator.generate(32);
             final var team = qm.createTeam("Test");
             final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
             final String hashedKey = HexFormat.of().formatHex(digest.digest(ApiKey.getOnlyKeyAsBytes(clearKey)));
@@ -217,8 +219,8 @@ public class ApiKeyAuthenticationServiceTest {
             apiKey.setCreated(new Date());
             apiKey.setTeams(List.of(team));
             qm.persist(apiKey);
+            apiKey.setClearTextKey(clearKey);
         }
-        apiKey.setKey(clearKey);
         return apiKey;
     }
 }
